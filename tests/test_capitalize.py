625ccecf744b437eb9e465550d59932dd95c4975
SilAres <haritonov.e.a@outlook.com>
2022-08-28 11:00:21 +0300

    удалены коменты

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,11 +1,3 @@
-# Нажмите кнопку Run чтобы запустить тесты.
-
-# Файлы с исходным кодом можно посмотреть на вкладке "Files":
-# src/capitalize.py        - тестируемая функция
-# tests/test_capitalize.py - тесты функции
-
-# Попробуйте изменять код функции/тестов, запуская проверки заново.
-
 from capitalize import capitalize
 
 if capitalize('hello') != 'Hello':
